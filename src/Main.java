a6f069159ddd4728ea725d227b31b4b8d71b9e2f
Max Levinson <maxl77@nycstudents.net>
2026-03-30 08:46:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/brick_layout");
+        ArrayList<Brick> bricks = new ArrayList<Brick>();
+        for (String line : fileData) {
+            String[] points = line.split(",");
+            int start = Integer.parseInt(points[0]);
+            int end = Integer.parseInt(points[1]);
+            Brick b = new Brick(start, end);
+            bricks.add(b);
+        }
+
+
+        System.out.println(partOne(bricks));
+        System.out.println(partTwo(bricks));
+    }
+
+    private static int partOne(ArrayList<Brick> bricks) {
+        int total = 0;
+        for (Brick b : bricks) {
+            total += b.getEnd() - b.getStart();
+        }
+        return total;
+    }
+
+    private static int partTwo(ArrayList<Brick> bricks) {
+        // Implement me!
+        return 0;
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        File f = new File(fileName);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+        ArrayList<String> fileData = new ArrayList<String>();
+        while (s.hasNextLine())
+            fileData.add(s.nextLine());
+
+        return fileData;
+    }
+}
